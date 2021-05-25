3f5757faf6473e0726c4c15998ba66bf083256dd
Alexandra <arbeuerman@gmail.com>
2021-05-25 10:53:37 -0500

    practice toy tales take 3

--- src/App.js
+++ src/App.js
@@ -5,11 +5,17 @@ import Header from './components/Header'
 import ToyForm from './components/ToyForm'
 import ToyContainer from './components/ToyContainer'
 
+const toysUrl = 'http://localhost:3000/toys';
+const headers = {
+  'Content-Type': 'application/json',
+  Accepts: 'application/json'
+}
 
 class App extends React.Component{
 
   state = {
-    display: false
+    display: true,
+    toys: []
   }
 
   handleClick = () => {
@@ -19,20 +25,60 @@ class App extends React.Component{
     })
   }
 
+  //get all toys and render right after initial render
+  //also set state for toys so toys can be passed around
+  componentDidMount() {
+    fetch(toysUrl)
+    .then(res => res.json())
+    .then(toys => this.setState({toys}))
+  }
+
+  createNewToy = (newToy) => {
+    newToy.likes = 0;
+    fetch(toysUrl, {
+      method: 'POST',
+      headers,
+      body: JSON.stringify(newToy)
+    })
+    .then(res => res.json())
+    .then(toy => this.setState({toys: [...this.state.toys, toy]}))
+    .catch(err => console.log(err));
+  }
+
+  deleteToy = (toyToDelete) => {
+    fetch(`${toysUrl}/${toyToDelete.id}`, {
+      method: 'DELETE',
+      headers,
+    })
+    .then(this.setState({toys: this.state.toys.filter(toy => toy !== toyToDelete)}))
+    .catch(err => console.log(err));
+  }
+
+  editToyLikes = (toyToEdit) => {
+    fetch(`${toysUrl}/${toyToEdit.id}`, {
+      method: 'PATCH',
+      headers,
+      body: JSON.stringify({likes: toyToEdit.likes + 1})
+    })
+    .then(res => res.json())
+    .then(editedToy => this.setState({toys: this.state.toys.map(toy => toy.id ===toyToEdit.id ? editedToy : toy)}))
+    .catch(err => console.log(err));
+  }
+
   render(){
     return (
       <>
         <Header/>
         { this.state.display
             ?
-          <ToyForm/>
+          <ToyForm createNewToy={this.createNewToy}/>
             :
           null
         }
         <div className="buttonContainer">
           <button onClick={this.handleClick}> Add a Toy </button>
         </div>
-        <ToyContainer/>
+        <ToyContainer toys={this.state.toys} deleteToy={this.deleteToy} addLikes={this.editToyLikes}/>
       </>
     );
   }
--- src/components/ToyForm.jsx
+++ src/components/ToyForm.jsx
@@ -2,14 +2,31 @@ import React, { Component } from 'react';
 
 class ToyForm extends Component {
 
+  //add state so form can be controlled
+  state = {
+    name: '',
+    image: ''
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.props.createNewToy(this.state);
+    this.setState({name: '', image: ''})
+  }
+
+  handleStateChange = (event) => {
+    this.setState({[event.target.name]: event.target.value})
+  }
   render() {
     return (
       <div className="container">
-        <form className="add-toy-form">
+        <form className="add-toy-form" onSubmit={this.handleSubmit}>
           <h3>Create a toy!</h3>
-          <input type="text" name="name" placeholder="Enter a toy's name..." className="input-text"/>
+          <input type="text" name="name" value={this.state.name} onChange={this.handleStateChange}
+          placeholder="Enter a toy's name..." className="input-text"/>
           <br/>
-          <input type="text" name="image" placeholder="Enter a toy's image URL..." className="input-text"/>
+          <input type="text" name="image" value={this.state.image} onChange={this.handleStateChange}
+          placeholder="Enter a toy's image URL..." className="input-text"/>
           <br/>
           <input type="submit" name="submit" value="Create New Toy" className="submit"/>
         </form>
